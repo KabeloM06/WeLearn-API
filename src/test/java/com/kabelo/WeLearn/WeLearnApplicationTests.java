5fce0fa432d84764ba0106569051c81814cce090
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2023-05-24 09:46:41 +0200

    Starter files

---
+++ src/test/java/com/kabelo/WeLearn/WeLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kabelo.WeLearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
