90800118cdeb7ca61d246d1595f44573d036d9b1
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2023-05-24 11:12:10 +0200

    method to GET one user

--- src/main/java/com/kabelo/WeLearn/user/UserResource.java
+++ src/main/java/com/kabelo/WeLearn/user/UserResource.java
@@ -1,6 +1,7 @@
 package com.kabelo.WeLearn.user;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,5 +19,11 @@ public class UserResource {
     @GetMapping("/users")
     public List<User> retrieveAllUsers(){
         return userDaoService.findAll();
+    }
+
+    // Get 1 user
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id){
+        return userDaoService.findOne(id);
     }
 }
